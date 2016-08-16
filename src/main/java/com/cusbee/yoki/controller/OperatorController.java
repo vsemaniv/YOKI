55952b033cbe0d0ce7318e5cbb921a405f2d8e37
Alesto <alesto911@gmail.com>
2016-08-16 14:41:36 +0300

    controller fix #2

--- src/main/java/com/cusbee/yoki/controller/OperatorController.java
+++ src/main/java/com/cusbee/yoki/controller/OperatorController.java
@@ -5,6 +5,7 @@ import com.cusbee.yoki.dto.YokiResult.Status;
 import com.cusbee.yoki.entity.Order;
 import com.cusbee.yoki.entity.enums.CrudOperation;
 import com.cusbee.yoki.entity.enums.OrderStatus;
+import com.cusbee.yoki.model.IdModel;
 import com.cusbee.yoki.model.OrderModel;
 import com.cusbee.yoki.service.OrderService;
 import com.wordnik.swagger.annotations.ApiClass;
@@ -68,14 +69,14 @@ public class OperatorController {
 	}*/
 
 	@RequestMapping(value="closeOrder", method=RequestMethod.POST)
-	public YokiResult<Order> closeOrder(@RequestParam("id")Long id) {
-		Order order = orderService.saveOrderStatus(id, OrderStatus.CLOSED);
+	public YokiResult<Order> closeOrder(@RequestBody IdModel idModel) {
+		Order order = orderService.saveOrderStatus(idModel.getId(), OrderStatus.CLOSED);
 		return new YokiResult<Order>(Status.SUCCESS, "Order was successfully closed", order);
 	}
 
 	@RequestMapping(value="setOrderInProgress", method=RequestMethod.POST)
-	public YokiResult<Order> setOrderInProgress(@RequestParam("id")Long id) {
-		Order order = orderService.saveOrderStatus(id, OrderStatus.IN_PROGRESS);
+	public YokiResult<Order> setOrderInProgress(@RequestBody IdModel idModel) {
+		Order order = orderService.saveOrderStatus(idModel.getId(), OrderStatus.IN_PROGRESS);
 		return new YokiResult<Order>(Status.SUCCESS, "Order is now in progress", order);
 	}
 }
---
+++ src/main/java/com/cusbee/yoki/model/IdModel.java
@@ -0,0 +1,14 @@
+package com.cusbee.yoki.model;
+
+public class IdModel {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
