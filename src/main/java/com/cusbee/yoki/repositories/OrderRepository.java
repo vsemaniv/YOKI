b19c0f3d4f6f2defcd77d97f7a2b09e1278d97b0
Alesto <alesto911@gmail.com>
2016-08-31 14:46:23 +0300

    history fix

--- src/main/java/com/cusbee/yoki/repositories/OrderRepository.java
+++ src/main/java/com/cusbee/yoki/repositories/OrderRepository.java
@@ -24,9 +24,9 @@ public interface OrderRepository  extends JpaRepository<Order, Long>{
 	@Query(value="SELECT o.* FROM orders o WHERE o.order_date BETWEEN ?1 AND ?2", nativeQuery=true)
 	List<Order> getOrderHistory(String startDate, String endDate);
 
-	@Query(value="SELECT o.* FROM orders o WHERE o.client_id = ?3 AND o.order_date BETWEEN ?1 AND ?2", nativeQuery=true)
+	@Query(value="SELECT o.* FROM orders o WHERE o.client_phone = ?3 AND o.order_date BETWEEN ?1 AND ?2", nativeQuery=true)
 	List<Order> getOrderHistory(String startDate, String endDate, String client);
 
-	@Query(value="SELECT o.* FROM orders o WHERE o.client_id = ?1", nativeQuery=true)
+	@Query(value="SELECT o.* FROM orders o WHERE o.client_phone = ?1", nativeQuery=true)
 	List<Order> getOrderHistory(String client);
 }
