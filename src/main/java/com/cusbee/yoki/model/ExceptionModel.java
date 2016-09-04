36bf20200cc02eadf17d9c21d57a5a5a15dee57e
Alesto <alesto911@gmail.com>
2016-09-05 01:38:50 +0300

    exception handler fix #3

--- src/main/java/com/cusbee/yoki/model/ExceptionModel.java
+++ src/main/java/com/cusbee/yoki/model/ExceptionModel.java
@@ -4,8 +4,9 @@ package com.cusbee.yoki.model;
  * Created on 05.09.2016.
  */
 public class ExceptionModel {
-    private String message;
 
+    private String name;
+    private String message;
     private StackTraceElement[] elements;
 
     public ExceptionModel(Exception e) {
@@ -13,12 +14,21 @@ public class ExceptionModel {
         while(first.getCause() != null && first.getCause() != first) {
             first = first.getCause();
         }
+        this.name = first.getClass().getName();
         this.message = first.getMessage();
         this.elements = first.getStackTrace();
 
     }
 
     public ExceptionModel() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getMessage() {
